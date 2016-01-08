5c8cf9f79f967e58f7e932571583ac871d98a0e7
Yuvprime <yuvztikode@gmail.com>
2016-01-08 17:09:29 +0530

    Added method for ImageLoader

--- app/src/main/java/com/example/yuvaraj/myapplication/volley/AppController.java
+++ app/src/main/java/com/example/yuvaraj/myapplication/volley/AppController.java
@@ -5,6 +5,7 @@ import android.text.TextUtils;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 public class AppController extends Application {
@@ -14,6 +15,7 @@ public class AppController extends Application {
     private RequestQueue mRequestQueue;
 
     private static AppController mInstance;
+    private ImageLoader imageLoader;
 
     @Override
     public void onCreate() {
@@ -48,6 +50,18 @@ public class AppController extends Application {
             mRequestQueue.cancelAll(tag);
         }
     }
+
+    public ImageLoader getImageLoader()
+    {
+        getRequestQueue();
+        if (imageLoader == null)
+        {
+            imageLoader = new ImageLoader(this.mRequestQueue, new LruBitmapCache());
+        }
+        return this.imageLoader;
+    }
+
+
 }
 
 
