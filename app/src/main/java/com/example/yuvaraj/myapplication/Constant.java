10368d382d39f0bb26133ac4f584d23f5c9210c6
Yuvprime <yuvztikode@gmail.com>
2016-01-08 16:53:58 +0530

    Updated site url

--- app/src/main/java/com/example/yuvaraj/myapplication/Constant.java
+++ app/src/main/java/com/example/yuvaraj/myapplication/Constant.java
@@ -2,10 +2,10 @@ package com.example.yuvaraj.myapplication;
 
 public class Constant {
 
-    public static String site_url = "http://www.webservice.site90.net";
-    public static String updates_url = site_url + "/updates/lists";
-    public static String image_path = site_url + "/assets/images/";
-    public static String gallery_url = site_url + "/gallery/lists";
+    public static String site_url = "http://www.webservice.site90.net/";
+    public static String updates_url = site_url + "updates/lists";
+    public static String image_path = site_url + "assets/images/";
+    public static String gallery_url = site_url + "gallery/lists";
 
     // Php Application URL to store Reg ID created
     static final String APP_SERVER_URL = "http://webservice.site90.net/gcm.php?shareRegId=true";
